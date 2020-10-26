5e1b760d079825a1058e5752ca9a41c7fabcd5df
wang dongxiang <979063900@qq.com>
2020-10-26 22:35:13 +0800

    movie_api

--- vueapp/src/views/movie/Movie.vue
+++ vueapp/src/views/movie/Movie.vue
@@ -1,18 +1,35 @@
 <template>
   <div>
-    <h1>movie</h1>
+    <ul>
+      <li class="movie" v-for="(movie) in movieList" :key="movie.id">
+        <div class="movie-img">
+          <img :src="movie.images.medium" alt="">
+        </div>
+        <div class="movie-info">
+          <div class="movie-info-title">{{movie.title}}</div>
+          <div>观众评：<span class="movie-info-average">{{movie.rating.average}}</span></div>
+          <div class="movie-info-star">主演：<span v-for="item in movie.casts" :key="item.id">{{item.name}}&nbsp;</span></div>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
+  data() {
+    return {
+      movieList: [],
+    };
+  },
   beforeCreate() {
-    let url =
-      "https://bird.ioliu.cn/v2?url=http://api.douban.com/v2/movie/new_movies?apikey=0df993c66c0c636e29ecbb5344252a4a";
-    axios.get(url,{responseType: "blob"}).then((response) => {
-      console.log(response);
-    });
+    axios
+      .get("/api/new_movies?apikey=0df993c66c0c636e29ecbb5344252a4a")
+      .then((res) => {
+        console.log("proxy", res.data.subjects);
+        this.movieList = res.data.subjects;
+      });
   },
   created() {
     this.$emit("swithTab", "movie");
@@ -21,4 +38,34 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.movie{
+  display: flex;
+  padding: 0.2rem;
+  border-bottom: 0.02rem solid #ccc;
+  &-img{
+    flex-grow: 1;
+    width: 0;
+    img{
+      width: 100%;
+    }
+  }
+  &-info{
+    flex-grow: 3;
+    width: 0;
+    margin-left: 0.2rem;
+    &-title{
+      color: #333;
+      font-weight: 700;
+      font-size: 0.34;
+    }
+    &-average{
+      font-weight: 700;
+      color: #faaf00;
+    }
+    &-start{
+      color: #666;
+      font-size: 0.26rem;
+    }
+  }
+}
 </style>
--- vueapp/vue.config.js
+++ vueapp/vue.config.js
@@ -1,6 +1,26 @@
 module.exports = {
-    devServer: {
-      // proxy: 'http://localhost:4000'
-    },
-    lintOnSave: false
-  }
+  devServer: {
+    // overlay: { // 让浏览器 overlay 同时显示警告和错误
+    //   warnings: true,
+    //   errors: true
+    // },
+    // open: false, // 是否打开浏览器
+    // host: "localhost",
+    // port: "8080", // 代理断就
+    // https: false,
+    // hotOnly: false, // 热更新
+    proxy: {
+      "/api": {
+        target:
+          "https://api.douban.com/v2/movie", // 目标代理接口地址
+        secure: false,
+        changeOrigin: true, // 开启代理，在本地创建一个虚拟服务端
+        // ws: true, // 是否启用websockets
+        pathRewrite: {
+          "^/api": "/"
+        }
+      }
+    }
+  },
+  lintOnSave: false,
+};
